d4824eb7944390c5888643b9d1cb365083fbd9c5
kasperBuurDk <kasperbuur@gmail.com>
2023-06-15 22:29:03 +0200

    starting with som angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import {MatCalendarCellClassFunction } from '@angular/material/datepicker';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  
 })
 export class AppComponent {
   title = 'Playground';
+
+    dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+      // Only highligh dates inside the month view.
+      if (view === 'month') {
+        const date = cellDate.getDate();
+  
+        // Highlight the 1st and 20th day of each month.
+        return date === 1 || date === 20 ? 'example-custom-date-class' : '';
+      }
+  
+      return '';
+    };
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatFormFieldModule,
+     MatInputModule,
+      MatNativeDateModule,
+       MatDatepickerModule
+      ],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'da'},
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
